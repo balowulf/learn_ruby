1c73a42a21e6839c463593eaa713ee93658c6ae7
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-17 20:55:06 -0500

    good start on pig_latin, not yet working for multi-word strings

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,17 @@
-#write your code here
+def translate(str)
+    new_str = str.split(" ")
+    vowels = ["a", "e", "i", "o", "u"]
+    new_str.each do |word|
+        if vowels.include? word[0]
+            word << "ay"
+        else
+            # block below yields "errychay" from "cherry"
+            ary = word.split("")
+            newary = ary.shift(ary.index { |x| vowels.include? x })
+            newary << "ay"
+            ary << newary
+            return ary.join("")
+        end
+    end
+    new_str = new_str.join(" ")
+end
