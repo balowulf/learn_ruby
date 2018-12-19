8bf526735c93971d7fbc1e0bb674bc496512134b
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-19 10:15:24 -0500

    04, 05, 06 solutions

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,14 +1,22 @@
+# def translate(str)
+#     words = str.split(' ')
+#     words = words.map do |word|
+#         if word[0].chr == 'q' 
+#             parts = ['qu', word.slice(2, word.length - 2)]
+#         elsif word[1].chr == 'q'
+#             parts = [word[word.index('q') - 1] + "qu", word.slice(3, word.length - 2)]
+#         else 
+#             parts = word.split(/([aeiou].*)/)     
+#         end
+#         parts[1] + parts[0] + "ay"
+#     end
+#     words.join(' ')
+# end
+
 def translate(str)
-    words = str.split(' ')
-    words = words.map do |word|
-        if word[0].chr == 'q' 
-            parts = ['qu', word.slice(2, word.length - 2)]
-        elsif word[1].chr == 'q'
-            parts = [word[word.index('q') - 1] + "qu", word.slice(3, word.length - 2)]
-        else 
-            parts = word.split(/([aeiou].*)/)     
-        end
-        parts[1] + parts[0] + "ay"
-    end
-    words.join(' ')
+    regex = '\A([(?:qu)||[^aeiouAEIOU]]*)(.*)\z'
+    str.split.each { |word|
+        word.match regex
+        word.replace "#{$2}#{$1}ay"
+    } * ' '
 end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,20 +1,31 @@
+# class Book
+#     def title=(str)
+#         words = str.split
+#         new_title = ""
+#         keywords = ["a", "an", "and", "the", "in", "of"]
+#         words.map { |word|
+#             if keywords.include? word
+#                 new_title << word
+#             else
+#                 new_title << word.capitalize
+#             end
+#             new_title << " "
+#         }
+#         @title = new_title.strip
+#     end
+
+#     def title
+#         @title
+#     end
+# end
+
 class Book
-    def title=(str)
-        words = str.split
-        new_title = ""
-        keywords = ["a", "an", "and", "the", "in", "of"]
-        words.map { |word|
-            if keywords.include? word
-                new_title << word
-            else
-                new_title << word.capitalize
-            end
-            new_title << " "
-        }
-        @title = new_title.strip
-    end
+    attr_writer :title
 
     def title
-        @title
+        ignore_words = %w{the a an and in on of at or}
+        @title.split.each_with_index { |word, n| 
+            word.capitalize! unless ignore_words.include? word and n > 0
+        } * ' '
     end
 end
