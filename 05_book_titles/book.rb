b34ceea52c41c06feb28b5bdbbaf57edb791fe54
Chris Balow Jr <cmbalow@illuminateed.net>
2018-12-18 20:17:43 -0500

    works for everything except "the man in the iron mask"

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+    def title=(str)
+        words = str.split
+        new_title = ""
+        keywords = ["a", "an", "and", "the", "in", "of"]
+        words.map { |word|
+            if keywords.include? word
+                new_title << word
+            else
+                new_title << word.capitalize
+            end
+            new_title << " "
+        }
+        @title = new_title.strip
+    end
+
+    def title
+        @title
+    end
 end
